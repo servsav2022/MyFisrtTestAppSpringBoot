722f00e9baf9aa14c9caa511eb9d8b6b97d25db8
alexandr <svaluhinav@yandex.ru>
2023-09-08 10:50:19 +0500

    2 course Lab 1

---
+++ src/main/java/ru/servsav/myfisrttestappspringboot/MyFisrtTestAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.servsav.myfisrttestappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFisrtTestAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFisrtTestAppSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/servsav/myfisrttestappspringboot/MyFisrtTestAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.servsav.myfisrttestappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFisrtTestAppSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
