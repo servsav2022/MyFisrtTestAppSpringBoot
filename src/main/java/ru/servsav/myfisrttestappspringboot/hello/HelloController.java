b1bb2dc7ac56acf60fe3215882003fb2e2bdc81d
alexandr <svaluhinav@yandex.ru>
2023-09-13 10:56:55 +0500

    2 курс лаб1 финальная версия

--- src/main/java/ru/servsav/myfisrttestappspringboot/hello/HelloController.java
+++ src/main/java/ru/servsav/myfisrttestappspringboot/hello/HelloController.java
@@ -13,36 +13,51 @@ import java.util.UUID;
 public class HelloController {
     ArrayList<String> list;
     HashMap<String, String> hashMap;
+
+    //Реализация простейшего Spring Boot Application
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name",
             defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
+    //Реализация дополнительного функционала
+
+    //1. Создайте GET-метод updateArrayList(String s),
+    // который по url “/update-array” принимает аргумент и записывает его в ArrayList<String >,
+    // реализуйте логику - если это первый вызов метода, то создается пустой ArrayList,
+    // если он не пустой, то туда записывается значение.
     @GetMapping("/update-array")
-    public String updateArrayList(@RequestParam(value = "s",
-            defaultValue = "") String s) {
+    public String updateArrayList(@RequestParam(value = "s", defaultValue = "") String s) {
         if (list == null) {
             list = new ArrayList<String>();
         }
         list.add(s);
-        return String.format("В ArrayList добавлен элемент %s",s);
+        return String.format("В ArrayList добавлен элемент: %s",s);
     }
+
+    //2. Создайте GET метод showArrayList(),
+    // который по url “/show-array” возвращается все элементы хранящиеся в ArrayList <String >, созданном в п. 1
     @GetMapping("/show-array")
     public String showArrayList(String string) {
         String format = null;
         if (list != null) {
             format = String.format("Содержимое ArrayList: %s", list.toString());
-        }
+        } else format="В ArrayList нет элементов \n";
         return format;
     }
+
+   // 3. Создайте GET-метод updateHashMap(String s),
+    // который по url “/update-map” принимает аргумент и записывает его в HashMap<String, String>,
+    // реализуйте логику - если это первый вызов метода, то создается пустой HashMap,
+    // если он не пустой, то туда записывается значение.
 
     // генерация уникального ключа на основе значения для HashMap<String, String>
     private static String generateUniqueKey(String value) {
         return value + "_" + UUID.randomUUID().toString();
     }
     @GetMapping("/update-map")
-    public String updateHashMap(@RequestParam(value = "s",
-            defaultValue = "") String s) {
+    public String updateHashMap(@RequestParam(value = "s", defaultValue = "") String s) {
         if (hashMap == null) {
             hashMap = new HashMap<>();
         }
@@ -56,6 +71,9 @@ public class HelloController {
 
         return String.format("В HashMap добавлен элемент %s",s);
     }
+
+    //4. Создайте GET метод showHashMap (),
+    // который по url “/show-map” возвращается все элементы хранящиеся в HashMap<String, String>, созданном в п. 3
     @GetMapping("/show-map")
     public String showHashMap(String s) {
         String format ="";
@@ -68,8 +86,22 @@ public class HelloController {
                  format = format + String.format
                          ("[ Ключ: " + key + ", Значение: " + val +" ]\n");
             }
+        }else format="В hashMap нет элементов \n";
+        return format;
+    }
 
-        }
+    //5. Создайте GET метод showAllLenght (),
+    // который по url “/show-all-lenght” возвращает текст,
+    // в котором указано количество элементов в ArrayList и HashMap
+    @GetMapping("/show-all-length")
+    public String showAllLength(String s) {
+        String format ="";
+        if (list != null ) {
+         format="Кол-во элементов в ArrayList = " + list.size() + " \n";
+        } else format="В ArrayList нет элементов \n";
+        if (hashMap != null ) {
+            format=format + "Кол-во элементов в hashMap = " + hashMap.size() + " \n";
+        } else format=format + "В hashMap нет элементов \n";
         return format;
     }
 }
